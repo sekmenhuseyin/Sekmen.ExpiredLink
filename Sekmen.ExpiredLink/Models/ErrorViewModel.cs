7332ce6dd5404b53ca8e48ce73faac6c83ab9561
Hüseyin Sekmenoğlu <huseyin@sekmen.dev>
2021-01-25 17:34:19 +0300

    Add project files.

---
+++ Sekmen.ExpiredLink/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sekmen.ExpiredLink.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
